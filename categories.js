dcabec3f09c5d1e5486542e1c15ba52e8eabb30b
zprintzanzibar <zprint.znz@gmail.com>
2025-12-25 14:46:03 +0300

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,121 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2865
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 const CATEGORIES = \{\
+\
+  "PVC": \{\
+    label: "PVC",\
+    subs: \{\
+      "CLEAR": "Clear",\
+      "PRC": "Print & Cut",\
+      "UV": "UV",\
+      "UVPC": "UV Print & Cut",\
+      "CUT": "Cutting",\
+      "PLN": "Plain",\
+      "GLDC": "Gold Cutting",\
+      "GLDP": "Gold Plain",\
+      "ECO": "Eco",\
+      "CLRPRC": "Clear Print & Cut",\
+      "FRPL": "Frost Plain",\
+      "FRCUT": "Frost Cut",\
+      "FRPRC": "Frost Print & Cut",\
+      "MAG": "Magnet"\
+    \}\
+  \},\
+\
+  "EMB": \{\
+    label: "Embroidery",\
+    subs: \{\
+      "STD": "Standard",\
+      "PRM": "Premium"\
+    \}\
+  \},\
+\
+  "CAN": \{\
+    label: "Canvas",\
+    subs: \{\
+      "COT": "Cotton",\
+      "UV": "UV",\
+      "ECO": "Eco",\
+      "OTH": "Other"\
+    \}\
+  \},\
+\
+  "BAN": \{\
+    label: "Banner",\
+    subs: \{\
+      "UV": "UV",\
+      "ECO": "Eco",\
+      "TD": "Tear Drop",\
+      "FECO": "Flag Eco",\
+      "FUV": "Flag UV",\
+      "POP": "POP",\
+      "STD": "Standard",\
+      "PRM": "Premium"\
+    \}\
+  \},\
+\
+  "MAP": \{\
+    label: "Map",\
+    subs: \{\
+      "NRM": "Normal",\
+      "PP": "PP",\
+      "POP": "POP"\
+    \}\
+  \},\
+\
+  "SUB": \{\
+    label: "Sublimation",\
+    subs: \{\
+      "SUB": "Sublimation"\
+    \}\
+  \},\
+\
+  "DTF": \{\
+    label: "DTF",\
+    subs: \{\
+      "DTF": "DTF"\
+    \}\
+  \},\
+\
+  "CON": \{\
+    label: "Contra",\
+    subs: \{\
+      "UV": "UV",\
+      "ECO": "Eco",\
+      "PLN": "Plain"\
+    \}\
+  \},\
+\
+  "FLT": \{\
+    label: "Flatbed",\
+    subs: \{\
+      "UVD": "UV_DTF",\
+      "PEN": "Pen",\
+      "NOTE": "Notebook",\
+      "ACR": "Acrylic"\
+    \}\
+  \},\
+\
+  "PUB": \{\
+    label: "Publications",\
+    subs: \{\
+      "FLY": "Flyer",\
+      "BR": "Brochure",\
+      "BC": "Business Card",\
+      "OTH": "Other"\
+    \}\
+  \},\
+\
+  "CUS": \{\
+    label: "Custom",\
+    subs: \{\
+      "CUS": "Custom Input"\
+    \}\
+  \}\
+\
+\};}
